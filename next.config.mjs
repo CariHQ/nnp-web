fada07da8af53600a19cc4dff4b0534590ad2238
Kenroy George <kenroy@garakami.com>
2025-02-12 05:37:17 +0100

    chore: update next.js config for assets

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@
 //   // ignore error
 // }
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NEXT_PUBLIC_NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
